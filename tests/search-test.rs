5bece570e514692138c9ecd6ee2118ca3bc852f6
gitkeniwo <kennethwwwww@gmail.com>
2024-06-09 18:13:17 +0200

    update integration test

--- tests/search-test.rs
+++ tests/search-test.rs
@@ -21,3 +21,32 @@ fn search_works_on_file() {
 
     assert_eq!(vec!["So this is how democracy dies, with thunderous applause"], config::search(&query, &contents));
 }
+
+
+
+#[test]
+fn case_sensitive() {
+    let query = "duct";
+    let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+
+    assert_eq!(vec!["safe, fast, productive."], config::search(query, contents));
+}
+
+#[test]
+fn case_insensitive() {
+    let query = "rUsT";
+    let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+    assert_eq!(
+        vec!["Rust:", "Trust me."],
+        config::search_case_insensitive(query, contents)
+    );
+}
